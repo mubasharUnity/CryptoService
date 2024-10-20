27ba49fc33b780c6aeb9c73b4753d670e4a6b817
DESKTOP-CO9DSGT\Mirza <mubashar437@gmail.com>
2024-10-20 06:23:10 +0500

    attached response routine to main server routine

--- tcpip_server.go
+++ tcpip_server.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net"
 )
 
 func HandleConnection(conn net.Conn) {
 	outChan := make(chan *OutMessage, 16)
+	ctx, cancel := context.WithCancel(context.Background())
+
 	defer func() {
+		cancel()
 		conn.Close()
 		for value := range outChan {
 			fmt.Println("drained buffer of id: ", len(value.inMessage.msgId))
@@ -19,7 +23,7 @@ func HandleConnection(conn net.Conn) {
 		}
 	}()
 
-	go SendResponses(conn, outChan)
+	go SendResponses(conn, outChan, ctx)
 
 	for {
 		msg := make([]byte, BUFFER_LENGTH)
@@ -50,10 +54,15 @@ func ProcessInputMessage(inMsg *Message, chOutWriteOnly chan<- *OutMessage) {
 	chOutWriteOnly <- &outMsg
 }
 
-func SendResponses(conn net.Conn, outputChan <-chan *OutMessage) {
+func SendResponses(conn net.Conn, outputChan <-chan *OutMessage, ctx context.Context) {
 	for {
-		responseMsg := <-outputChan
-		responseBuffer := responseMsg.FormResponseBuffer()
-		conn.Write(responseBuffer)
+		select {
+		case responseMsg := <-outputChan:
+			responseBuffer := responseMsg.FormResponseBuffer()
+			conn.Write(responseBuffer)
+		case <-ctx.Done():
+			fmt.Println("Context canceled.")
+			return
+		}
 	}
 }
